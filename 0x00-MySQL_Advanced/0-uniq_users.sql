8242530639fe4dc200f74753eb5fa3dece9f17b7
aellun <okello.k.otieno@gmail.com>
2024-08-12 21:02:19 +0300

    revised

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -5,7 +5,8 @@
 -- If the table already exists, the script will not fail
 
 CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255)
+    name VARCHAR(255),
+    PRIMARY KEY (id)
 );
