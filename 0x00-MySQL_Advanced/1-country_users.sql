4897b2c430279da0869b7aa17225fb031b8a4366
aellun <okello.k.otieno@gmail.com>
2024-08-12 20:55:39 +0300

    updated

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -7,8 +7,8 @@
 -- If the table already exists, the script will not fail
 
 CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255),
-    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
-) ENGINE=InnoDB;
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    email varchar(255) NOT NULL UNIQUE,
+    name varchar(255),
+    country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
+);
