f7f1950029c90aa674c15f957c4287fea0c86013
aellun <okello.k.otieno@gmail.com>
2024-08-17 23:05:09 +0300

    list of school having specific topic

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+'''Return the list of schools that have a specific topic.
+    Returns:
+    list: A list of schools (documents) that have the specific topic.
+'''
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    Parameters:
+    mongo_collection (pymongo.collection.Collection)
+    topic (str):The topic to search for in the schools' topics field.
+    """
+    schools = mongo_collection.find({"topics": topic})
+    return list(schools)
