d164559dbcf274dd881aba574e1cfd5896c20497
aellun <okello.k.otieno@gmail.com>
2024-08-17 17:24:22 +0300

    revised

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
--- SQL script that creates a table users with
-    --id,email,name
+-- A SQL script that creates a table (called users) with following fields
+-- id, email, name
 CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255)
-);
+	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	email varchar(255) NOT NULL UNIQUE,
+	name varchar(255)
+)
