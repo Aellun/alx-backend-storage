c3b0c09dfb866536db75a4862f02020dec642720
aellun <okello.k.otieno@gmail.com>
2024-08-17 17:09:43 +0300

    updated

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,8 +1,10 @@
+echo "
 -- SQL script that creates a table users with
-    --id,email,name
+-- id, email, name
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
     PRIMARY KEY (id)
 );
+"
